e2c6c35933865a63def552300e7ee18007de2ce4
yuricca <yuricca531@gmail.com>
2021-10-26 16:40:32 +0900

    軽微な修正

--- app/controllers/nails_controller.rb
+++ app/controllers/nails_controller.rb
@@ -6,6 +6,7 @@ class NailsController < ApplicationController
 
   def move_to_index
     unless user_signed_in?
-    redirect_to action: :index
+      redirect_to action: :index
+    end
   end
 end
