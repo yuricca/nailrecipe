56fd41fe99d8a4c8a2302faa73980945269135ab
yuricca <yuricca531@gmail.com>
2021-10-28 05:21:26 +0900

    投稿フォーム（基本）
    基本的な入力フォームの作成
    ActiveStorageの導入

--- app/controllers/nails_controller.rb
+++ app/controllers/nails_controller.rb
@@ -4,6 +4,14 @@ class NailsController < ApplicationController
   def index
   end
 
+  def new
+    
+  end
+
+  def create
+    
+  end
+
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
--- app/models/nail.rb
+++ app/models/nail.rb
@@ -1,6 +1,5 @@
 class Nail < ApplicationRecord
   belongs_to :user
   has_many :comments
-
-  
+  has_one_attached :image
 end
