84eff5d2826a2dda3bae1a43614fa132c85b9358
yuricca <yuricca531@gmail.com>
2021-10-29 05:20:40 +0900

    new,createアクションの定義

--- app/controllers/nails_controller.rb
+++ app/controllers/nails_controller.rb
@@ -2,14 +2,26 @@ class NailsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
+    @nails = Nail.all.order('create_at DESC')
   end
 
   def new
-    
+    @nail = Nail.new
   end
 
   def create
-    
+    @nail = Nail.new(nail_params)
+    if @nail.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def nail_params
+    params.require(:nail).permit(:title, :detail, :thumb, :index_finger, :middle_finger, :ring_finger, :little_finger, image[]).merge(user_id: current_use.id)    
   end
 
   def move_to_index
